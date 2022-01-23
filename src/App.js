153da7315ece94c179217b88b5fdc407e4877e46
Aniruddh Dev Upadhyay <aniruddh622003@gmail.com>
2022-01-23 22:48:00 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Sidebar from "./components/Sidebar";
 function App() {
   return (
     <div className="app">
+      <div className="app__bg"></div>
       <div className="app__body">
         <Sidebar />
         <Chat />
