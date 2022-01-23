cfcdcf26a0759b3d1b6ba96a25c0043ab15c225e
Aniruddh Dev Upadhyay <aniruddh622003@gmail.com>
2022-01-24 02:22:03 +0530

    add new room

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -5,11 +5,21 @@ import {
   Search,
 } from "@mui/icons-material";
 import { Avatar, IconButton, InputAdornment, TextField } from "@mui/material";
+import { onSnapshot, collection } from "firebase/firestore";
 import React from "react";
+import db from "../../firebase";
 import SidebarChat from "../SidebarChat";
 import classes from "./index.module.css";
 
 const Sidebar = () => {
+  const [rooms, setRooms] = React.useState([]);
+
+  React.useEffect(() => {
+    onSnapshot(collection(db, "rooms"), (snap) =>
+      setRooms(snap.docs.map((doc) => ({ id: doc.id, data: doc.data() })))
+    );
+  }, []);
+
   return (
     <div className={classes.sidebar}>
       <div className={classes.header}>
@@ -43,16 +53,9 @@ const Sidebar = () => {
       </div>
       <div className={classes.chat}>
         <SidebarChat addNewButton />
-        <SidebarChat />
-        <SidebarChat />
-        <SidebarChat />
-        <SidebarChat />
-        <SidebarChat />
-        <SidebarChat />
-        <SidebarChat />
-        <SidebarChat />
-        <SidebarChat />
-        <SidebarChat />
+        {rooms.map((room) => (
+          <SidebarChat key={room.id} name={room.data.name} />
+        ))}
       </div>
     </div>
   );
