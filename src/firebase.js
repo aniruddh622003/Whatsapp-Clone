cd2a837966959b7512be91c997e02d82339dd5e6
Aniruddh Dev Upadhyay <aniruddh622003@gmail.com>
2022-01-24 01:52:21 +0530

    UI done

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyD1uq3xE_W-OsXwA_jHLgkZjhognbEBn7w",
   authDomain: "whatsapp-clone-react-a3bba.firebaseapp.com",
@@ -6,3 +10,12 @@ const firebaseConfig = {
   messagingSenderId: "296175928440",
   appId: "1:296175928440:web:b4692a80ea15d8c320198d",
 };
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore();
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
